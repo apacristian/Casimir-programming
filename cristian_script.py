d467904ee425bfa6479fd5c4c2a655bb2103f2f9
cristian <cristian@example.com>
2018-11-07 16:30:21 +0100

    20181107-1630

--- cristian_script.py
+++ cristian_script.py
@@ -1,14 +1,13 @@
 #here I will write script to convert OD curve data to matrix that will be analysed
 
-print(f'This files will be analysed {data}')
-
-
-def transform_csv_to_matrix(file):	
+def transform():	
 	data=[]
-	get_files=
+	get_files=0
 	while get_files!="no":
 		get_files=input('Which files you want to analyze?("no" if no more)')
 		data.append(get_files)
-	return data
-
+	data=data[:-1]
+	return  data
+	
+print (transform())
 
