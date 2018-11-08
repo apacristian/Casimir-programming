d919c4d1efa89da26c0131ff99e4969729a247eb
cristian <cristian@example.com>
2018-11-08 12:00:03 +0100

    20181108-1159

--- cristian_script.py
+++ cristian_script.py
@@ -1,6 +1,7 @@
 #here I will write script to convert OD curve data to matrix that will be analysed
- 
-def transform():	
+
+def transform():
+	"""This function will  ask for the name of all files to process and insert them in a list"""
 	data=[]
 	get_files=0
 	while get_files!="no":
@@ -8,6 +9,6 @@ def transform():
 		data.append(get_files)
 	data=data[:-1]
 	return  data
-	
+
 print (transform())
 
--- testing_scripts.py
+++ testing_scripts.py
@@ -1,3 +1,1 @@
-data = input('Which files you want to analyze?("no" if no more)')
 
-print(f'This files will be analysed {data}')
